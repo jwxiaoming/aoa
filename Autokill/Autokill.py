add4b6c8c3df6459bbb1b5894c684f975cf58a32
jwxiaoming <904158347@qq.com>
2021-09-12 08:41:19 +0800

    定点刷怪第一版
    1、添加自动模式
    2、添加高级模式

---
+++ Autokill/Autokill.py
@@ -0,0 +1,74 @@
+# coding:utf-8
+import pyautogui
+import time
+def mode_a():
+    attack_key = '2'
+    switch_key = 'tab'
+    turn_key = 'd'
+    turn_angle = 0.12
+    interval_time = 0.15
+    num = [5,4,3,2,1]
+
+    for i in num:
+        print('——请在 ' + str(i) + ' 秒内回到要挂机的窗口——')
+        time.sleep(1)
+
+    cishu = 0
+    while(True):
+        pyautogui.press(switch_key)
+        time.sleep(interval_time)
+        pyautogui.press(attack_key)
+        time.sleep(interval_time)
+        pyautogui.keyDown(turn_key)
+        time.sleep(turn_angle)
+        pyautogui.keyUp(turn_key)
+        time.sleep(interval_time)
+        cishu += 1
+        print('已完成自动攻击:'+str(cishu)+'次，按 Ctrl+C 退出，大马猴是骗子。')
+
+def mode_b():
+    attack_key = str(input('请输入攻击技能键：'))
+    switch_key = str(input('请输入怪物选择键(tab键请输入tab)：'))
+    turn_key = str(input('请输入转向键：'))
+    turn_angle = float(input('请输入转向角度值（建议0.1至0.15）：'))
+    interval_time = 0.15
+    num = [5,4,3,2,1]
+
+    for i in num:
+        print('——请在 ' + str(i) + ' 秒内回到要挂机的窗口——')
+        time.sleep(1)
+
+    cishu = 0
+    while(True):
+        pyautogui.press(switch_key)
+        time.sleep(interval_time)
+        pyautogui.press(attack_key)
+        time.sleep(interval_time)
+        pyautogui.keyDown(turn_key)
+        time.sleep(turn_angle)
+        pyautogui.keyUp(turn_key)
+        time.sleep(interval_time)
+        cishu += 1
+        print('已完成自动攻击:'+str(cishu)+'次，按 Ctrl+C 退出，大马猴是骗子。')
+
+def select_mode():
+    print('——————————————————————————————————————')
+    print('欢迎使用机甲世纪定点刷怪脚本，请选择以下两种模式开始~')
+    print('A：自动模式，请提前将攻击技能键放置在数字【2】')
+    print('B：高级模式，可自定义技能键与转向方向、角度')
+    print('——————————————————————————————————————')
+    mode = input('请输入A或B（不区分大小写）按回车，进行下一步：')
+    # mode_text = ['a','A','b','B']
+        
+    if mode == 'a'or mode=='A':
+        print('您已选择自动模式')
+        mode_a()
+    elif mode == 'b'or mode=='B':
+        print('您已选择高级模式')
+        mode_b()
+    else:
+        print('你输入的不是A或B,请重新输入。')
+        select_mode()
+        # exit()
+
+select_mode()
